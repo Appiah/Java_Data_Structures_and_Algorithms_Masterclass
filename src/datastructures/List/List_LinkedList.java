049de8af657d4899c46a1915830788680cdfc308
Appiah <emap218@gmail.com>
2024-04-19 16:55:53 +0000

    In a move to rather focus more on this 'project' labelled : "Java_Data_Structures_and_Algorithms_Masterclass" and rather zooming out of the "DataStructuresRevision",
    For starters we are bringing in "List" : AbstractSequentialList, ArrayList, CopyOnWriteArrayList, LinkedList and Vector.
    
    As a baseline ; there is also the Utilities package that has been added. Basically the Utilities package is there to help with refactoring all the methods in one place and improve the 'DRY'-ness of the code. DRY is Dont Repeat Yourself.

---
+++ src/datastructures/List/List_LinkedList.java
@@ -0,0 +1,37 @@
+package datastructures.List;
+
+import Utilities.Util;
+
+import java.util.LinkedList;
+
+public class List_LinkedList {
+    private final static String DATA_STRUCTURE_NAME = "LinkedList";
+
+    public static void main(String [] args) {
+
+        System.out.print("Data Structures Experimentation with : ");
+        System.out.print(DATA_STRUCTURE_NAME);
+
+        LinkedList<String> ll_str = new LinkedList</*Nothing here because we are using (a) Strings*/>();
+
+        ll_str.add("LL Item 00");
+        ll_str.add("LL Item 01");
+        ll_str.add("LL Item 02");
+        ll_str.add("LL Item 03");
+
+        //ll.add(7, "LL Item 06" );//this would throw an error
+        //Exception in thread "main" java.lang.IndexOutOfBoundsException: Index: 7, Size: 4
+
+        Util.showLn(ll_str.toString());
+
+        Util.showLn(ll_str.getFirst());
+
+
+
+        Util.showLn(ll_str.getLast());
+
+        //Methods / Functions :
+
+
+    }
+}
---
+++ src/datastructures/Utilities/Util.java
@@ -0,0 +1,13 @@
+package datastructures.Utilities;
+
+public class Util {
+
+    public static void show(String content_str){
+        System.out.print(content_str);
+    }
+
+    public static void showLn(String content_str){
+        show("\n"+content_str);
+    }
+
+}
