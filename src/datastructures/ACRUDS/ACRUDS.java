d920892ff3e424f0918501145a1712d7459cdff7
Appiah <emap218@gmail.com>
2024-04-19 17:36:56 +0000

    1. Brought in "Map" as a whole package. 2. Brought in "SCRUDA1", and renamed to "ACRUDS" : the datastructure that guarantees the time complexity is strictly 1 for any process in relation to a set of data been handled by ACRUDS.

---
+++ src/datastructures/ACRUDS/ACRUDS.java
@@ -0,0 +1,12 @@
+package datastructures.ACRUDS;
+
+public class ACRUDS {
+
+    /* ACRUDS : Advanced Creator Reader Update Delete structures at time complexity of 1
+    The ACRUDS datastructure is supposed to be a total embodiment of
+    a structure that offer a time complexity of 1 strictly at all times
+    for everything and anything that might be needed with a particular
+    set of data.
+    */
+
+}
---
+++ src/datastructures/Map/Map_LinkedHashMap.java
@@ -0,0 +1,15 @@
+package datastructures.Map;
+
+public class Map_LinkedHashMap {
+    //Get : 1
+    //ContainsKey : 1
+    //Next : 1
+    private final static String DATA_STRUCTURE_NAME = "";
+
+    public static void main(String [] args) {
+
+        System.out.print("Data Structures Experimentation with : ");
+        System.out.print(DATA_STRUCTURE_NAME);
+
+    }
+}
