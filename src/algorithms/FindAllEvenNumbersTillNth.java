ac54b63585c8fe445037718f539f7de3fe8e6deb
Appiah <emap218@gmail.com>
2024-04-23 17:06:49 +0000

    Clean up the variable names and also introduce the use of ArrayList in the solution . . .

--- src/algorithms/FindAllEvenNumbersTillNth.java
+++ src/algorithms/FindAllEvenNumbersTillNth.java
@@ -1,9 +1,6 @@
 package algorithms;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Queue;
+import java.util.*;
 
 public class FindAllEvenNumbersTillNth {
 
@@ -12,62 +9,64 @@ public class FindAllEvenNumbersTillNth {
         int n0_i = 0;
         int nth_i = 128;
 
-        System.out.println("The Event Nums With Zero In Place Of Odd Nums from "+n0_i+" to "+nth_i+" exclusive, are : "
+        System.out.println("The Event Nums With Zero In Place Of Odd Nums from "+n0_i+" to "+nth_i+" exclusive [using Arrays], are : \n"
                 + Arrays.toString(getEventNumsWithZeroInPlaceOfOddNums(n0_i, nth_i)) + "\n"
         );
 
-
-
-        System.out.println("The Event Nums With Tailing Zeros from "+n0_i+" to "+nth_i+" exclusive, are : "
-                + Arrays.toString(getEventNumsWithTailingZeros(n0_i, nth_i))
+        System.out.println("The Event Nums With Tailing Zeros from "+n0_i+" to "+nth_i+" exclusive [using Arrays], are : \n"
+                + Arrays.toString(getEventNumsWithTailingZeros(n0_i, nth_i)) +"\n"
         );
 
+        System.out.println("The Event Nums from "+n0_i+" to "+nth_i+" exclusive [using ArrayList], are : \n"
+                +  getEventNumsExact(n0_i, nth_i)
+        );
 
     }
 
     public static int [] getEventNumsWithZeroInPlaceOfOddNums(int n0_i, int nth){
 
-        int [] primeNums = new int[nth];
+        int [] evenNums = new int[nth];
+
+        int x = 0;
 
         for(int i = n0_i; i < nth; i++){
             if(i % 2 == 0){
-                primeNums[i] = i;
+                evenNums[x] = i;
             }
+            x++;
         }
 
-        return primeNums;
+        return evenNums;
     }
 
     public static int [] getEventNumsWithTailingZeros(int n0_i, int nth){
 
-        int [] primeNums = new int[nth];
+        int [] evenNums = new int[nth];
 
         int x = 0;
 
         for(int i = n0_i; i < nth; i++){
             if(i % 2 == 0){
-                primeNums[x] = i;
+                evenNums[x] = i;
                 x++;
             }
         }
 
-        return primeNums;
+        return evenNums;
     }
 
-    public static int [] getEventNumsExact(int n0_i, int nth){
+    public static ArrayList<Integer> getEventNumsExact(int n0_i, int nth){
 
-        int [] primeNums = new int[nth];
-
-        int x = 0;
+        ArrayList<Integer> evenNumsArrList = new ArrayList<>();
 
         for(int i = n0_i; i < nth; i++){
             if(i % 2 == 0){
-                primeNums[x] = i;
-                x++;
+                evenNumsArrList.add(i);
             }
+
         }
 
-        return primeNums;
+        return evenNumsArrList;
     }
 
 }
