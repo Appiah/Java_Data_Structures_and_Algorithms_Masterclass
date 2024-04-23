6d4eaf0dfd287d3b24460d0b55396f0f96afe8dd
Appiah <emap218@gmail.com>
2024-04-23 16:40:18 +0000

    have two methods : getEventNumsWithZeroInPlaceOfOddNums(...) , and getEventNumsExact(...)

--- src/algorithms/FindAllEvenNumbersTillNth.java
+++ src/algorithms/FindAllEvenNumbersTillNth.java
@@ -12,19 +12,35 @@ public class FindAllEvenNumbersTillNth {
         int n0_i = 0;
         int nth_i = 128;
 
-        System.out.println("The prime numbers from "+n0_i+" to "+nth_i+", are : "
-                + Arrays.toString(getEventNums(n0_i, nth_i))
+        System.out.println("The even numbers from "+n0_i+" to "+nth_i+", are : "
+                + Arrays.toString(getEventNumsExact(n0_i, nth_i))
         );
 
     }
 
-    public static int [] getEventNums(int n0_i, int nth){
+    public static int [] getEventNumsWithZeroInPlaceOfOddNums(int n0_i, int nth){
 
-        int [] primeNums = new int [nth];
+        int [] primeNums = new int[nth];
 
         for(int i = n0_i; i < nth; i++){
             if(i % 2 == 0){
                 primeNums[i] = i;
+            }
+        }
+
+        return primeNums;
+    }
+
+    public static int [] getEventNumsExact(int n0_i, int nth){
+
+        int [] primeNums = new int[nth];
+
+        int x = 0;
+
+        for(int i = n0_i; i < nth; i++){
+            if(i % 2 == 0){
+                primeNums[x] = i;
+                x++;
             }
         }
 
